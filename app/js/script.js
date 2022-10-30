d5a41f71ab5f056a23ec632031f2b9b92c1fa7f2
Lucas Andrade Cruz <ddlukas0@gmail.com>
2022-10-30 16:10:52 -0300

    Mudança no título e formatação

--- app/js/script.js
+++ app/js/script.js
@@ -20,21 +20,21 @@ const buildPokemonCards = (pokemons) => {
 
     pokemons.forEach(pokemon => {
         const card = `
-                <div class="pokemon-card" style="background-color: var(--${pokemon.type}-type);">
-                    <div class="flex w-full justify-between items-center">
-                        <h1 class="pokemon-name">${capitalize(pokemon.name)}</h1>
-    
-                        <p class="pokemon-id">#${String(pokemon.id).padStart(3, '0')}</p>
-                    </div>
-    
-                    <div class="pokemon-type">
-                        ${capitalize(pokemon.type)}
-                    </div>
-    
-                    <img class="pokeball" src="images/pokeball.png" alt="Pokeball">
-                    <img class="pokemon-image" src="${pokemon.image}" alt="${capitalize(pokemon.name)}">
+            <div class="pokemon-card" style="background-color: var(--${pokemon.type}-type);">
+                <div class="flex w-full justify-between items-center">
+                    <h1 class="pokemon-name">${capitalize(pokemon.name)}</h1>
+
+                    <p class="pokemon-id">#${String(pokemon.id).padStart(3, '0')}</p>
                 </div>
-            `
+
+                <div class="pokemon-type">
+                    ${capitalize(pokemon.type)}
+                </div>
+
+                <img class="pokeball" src="images/pokeball.png" alt="Pokeball">
+                <img class="pokemon-image" src="${pokemon.image}" alt="${capitalize(pokemon.name)}">
+            </div>
+        `
 
         pokedex.insertAdjacentHTML('beforeend', card)
     })
